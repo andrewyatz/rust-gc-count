f0b8994f9569487cb18eb52acf019e9cfb576fca
Nathan LeRoy <NLeRoy917@gmail.com>
2024-02-19 11:16:09 -0500

    better type hints

--- bindings/gc_count.pyi
+++ bindings/gc_count.pyi
@@ -13,4 +13,16 @@ class ChecksumResult:
     def __str__(self):
         return f"ChecksumResult(id={self.id}, length={self.length}, sha512={self.sha512}, md5={self.md5})"
 
-def checksum(file: str, verbose: bool) -> List[ChecksumResult]: ...
+def checksum(file: str, verbose: bool) -> List[ChecksumResult]:
+    """
+    Calculate the sequence lengths and checksums from a fasta file. It will
+    produce a list of ChecksumResult objects, each containing the following
+    
+    Sequence ID as it appears in the FASTA file
+    Sequence length
+    Refget ga4gh identifier (SQ.sha512t24u)
+    MD5 checksum hex encoded
+
+    :param file: The file to checksum
+    :param verbose: Whether to print out the progress
+    """

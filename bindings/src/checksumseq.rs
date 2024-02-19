16807b477eaec15c38f26219dbc72148d8e3ebc1
Nathan LeRoy <NLeRoy917@gmail.com>
2024-02-18 19:48:37 -0500

    add python bindings for checksum

---
+++ bindings/src/checksumseq.rs
@@ -0,0 +1,28 @@
+use pyo3::prelude::*;
+
+use seq_io::fasta::Reader;
+use rust_gc_count::checksum::process_sequence;
+use crate::models::PyChecksumResult;
+
+#[pyfunction]
+pub fn checksum(file: String, verbose: Option<bool>) -> Vec<PyChecksumResult> {
+    
+    let mut results = Vec::new();
+    let mut reader = Reader::from_path(file).unwrap();
+
+    let verbose = verbose.unwrap_or(false);
+
+    while let Some(record) = reader.next() {
+        let record = record.unwrap();
+        let (id, length, sha512, md5) = process_sequence(record, verbose);
+        
+        results.push(PyChecksumResult {
+            id,
+            length,
+            sha512,
+            md5
+        });
+    }
+
+    results
+}
--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,16 +1,13 @@
 use pyo3::prelude::*;
 
 pub mod checksumseq;
+pub mod models;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+use crate::checksumseq::checksum;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn bindings(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn gc_count(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(checksum, m)?)?;
     Ok(())
 }
